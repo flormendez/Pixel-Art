b2a074be3b35500071e609475a14650af1a50612
Florencia <florenciamendez93@gmail.com>
2018-10-06 11:21:28 -0300

    primer versión

--- Recursos/js/pixel-art.js
+++ Recursos/js/pixel-art.js
@@ -123,15 +123,12 @@ var nombreColores = [
 
 var paleta = document.getElementById("paleta");
 
-
 var grillapixeles = document.getElementById("grilla-pixeles");
 
 // Variable para guardar el elemento 'color-personalizado'.Es decir, el que se elige con la rueda de color.
 var colorPersonalizado = document.getElementById("color-personalizado");
 
-
 //--Funciones y eventos
-
 
 //Crea paleta para selección de colores
 var paletaDeColor = function() {
@@ -157,43 +154,93 @@ grilla();
 var cambiarIndicador = function(e) {
   document.getElementById("indicador-de-color").style.backgroundColor =
     e.target.style.backgroundColor;
-}
+};
 
 paleta.addEventListener("click", cambiarIndicador);
 
-
 //Función para colorear los pixeles del lienzo
-var pintarPixeles = function(e){
-  e.target.style.backgroundColor = document.getElementById("indicador-de-color").style.backgroundColor
+var pintarPixeles = function(e) {
+  e.target.style.backgroundColor = document.getElementById(
+    "indicador-de-color"
+  ).style.backgroundColor;
 };
 grillapixeles.addEventListener("click", pintarPixeles);
 
 //Cambia el segundo cuadrado que indica el color personalizado
-colorPersonalizado.addEventListener('change', function(){
+colorPersonalizado.addEventListener("change", function() {
   colorActual = colorPersonalizado.value;
-  document.getElementById("indicador-de-color").style.backgroundColor = colorActual;
+  document.getElementById(
+    "indicador-de-color"
+  ).style.backgroundColor = colorActual;
 });
 
-
-//Detecta si el Mouse está apretado 
-var haceClick = function(){
+//Detecta si el Mouse está apretado
+var haceClick = function() {
   console.log("click");
-  haceClick=true;
-}
-grillapixeles.addEventListener("mousedown", haceClick)
+  haceClick = true;
+};
+grillapixeles.addEventListener("mousedown", haceClick);
 
 //Detecta si el Mouse no está apretado
-var noHaceClick = function(){
+var noHaceClick = function() {
   console.log("no");
-  haceClick=false;
-}
-grillapixeles.addEventListener("mouseup", noHaceClick)
+  haceClick = false;
+};
+grillapixeles.addEventListener("mouseup", noHaceClick);
 
-//FuNción que si el mouse está apretado, pinta varios pixeles a la vez
-var pintarEnMovimiento = function(e){
+//Función que si el mouse está apretado, pinta varios pixeles a la vez
+var pintarEnMovimiento = function(e) {
   if (haceClick == true) {
-    pintarPixeles(e)
+    pintarPixeles(e);
   }
+};
+grillapixeles.addEventListener("mouseover", pintarEnMovimiento);
 
+//Función botón borrar todo
+$("#borrar").click(function() {
+  $pixeles = $("#grilla-pixeles div");
+  $pixeles.animate({ "background-color": "white" }, 1500);
+});
+
+function cargarSuperheroe(superheroe) {
+  var $pixeles = $("#grilla-pixeles div");
+  for (var i = 0; i < superheroe.length; i++) {
+    $pixeles[i].style.backgroundColor = superheroe[i];
+  }
 }
-grillapixeles.addEventListener("mouseover", pintarEnMovimiento)
+
+//Función para cargar superhéroe
+$(".imgs li img").click(function() {
+  var $superheroe = $(this).attr("id");
+  switch ($superheroe) {
+    case "batman":
+      cargarSuperheroe(batman);
+      break;
+    case "wonder":
+      cargarSuperheroe(wonder);
+      break;
+    case "flash":
+      cargarSuperheroe(flash);
+      break;
+    case "invisible":
+      cargarSuperheroe(invisible);
+      break;
+  }
+});
+
+// Abre una ventana para guardar nuestro arte en un archivo pixel-art.png
+function guardarPixelArt() {
+  html2canvas($("#grilla-pixeles") , {
+    onrendered: function(canvas) {
+      theCanvas = canvas;
+      canvas.toBlob(function(blob) {
+        saveAs(blob, "pixel-art.png");
+      });
+    }
+  });
+}
+
+//Funcionalidad boton guardar
+$("#guardar").click(function() {
+  guardarPixelArt();
+});
